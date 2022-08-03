1511788e2479f3f4d20f5db8990f549b2009f70f
Lucas <lucasluizss@live.com>
2022-08-03 23:19:05 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -170,7 +170,7 @@ class Main {
 
 	private validateSession(ctx: ZorkContext, callback: Function): void {
 		if (ctx.session?.translation) {
-			callback(this);
+			callback(ctx);
 		} else {
 			ctx.reply('You must select a language first! Please, check your menu options.');
 		}
